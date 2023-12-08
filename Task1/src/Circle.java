07542393c1d79fd456c9530402256950ca1c04c2
AllanChandler <cph-ac326@cphbusiness.dk>
2023-12-08 11:29:47 +0100

    Task1

---
+++ Task1/src/Circle.java
@@ -0,0 +1,17 @@
+public class Circle implements Shape{
+
+double radius;
+
+Circle(double radius){
+
+    this.radius = radius;
+
+}
+
+    public double calculateArea(){
+
+    return (Math.PI *(radius*radius));
+
+    }
+
+}
---
+++ Task1/src/Square.java
@@ -0,0 +1,22 @@
+public class Square implements Shape  {
+
+
+    double length;
+    double height;
+
+Square(double length, double height){
+
+    this.length = length;
+    this.height = height;
+
+}
+
+public double calculateArea(){
+
+    return height*length;
+
+}
+
+
+
+}
